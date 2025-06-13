0b0e3bd839410e881da2fe41382cb8f15a68bee8
Ayush8566 <kumarayush59567@gmail.com>
2025-06-14 00:46:40 +0530

    code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client' 
-import './index.css'  
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import { persistor, store } from './redux/store.js';
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>  
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
     </PersistGate>
-  </Provider>,
-)
+  </Provider>
+);
